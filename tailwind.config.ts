32376edce2caebe2bd4372e66dd1972fecacd142
hoshiyuu0320 <hoshiyuu0320@gmail.com>
2025-05-25 18:49:55 +0900

    パッケージの追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-lexend)', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
