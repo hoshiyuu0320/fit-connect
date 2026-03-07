4fbc56c41b55dbe37abc106f6a72a8d94e483177
hoshiyuu0320 <hoshiyuu0320@gmail.com>
2026-03-07 21:02:55 +0900

    テーマをローカルストレージに保存する対応

--- fit_connect_mobile/lib/core/providers/theme_provider.g.dart
+++ fit_connect_mobile/lib/core/providers/theme_provider.g.dart
@@ -6,12 +6,12 @@ part of 'theme_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$themeModeNotifierHash() => r'b8637a1582d105651690821c637adc410f2a675c';
+String _$themeModeNotifierHash() => r'bc8f5c43798d292ab8e413ca509dfea32de79fe0';
 
 /// See also [ThemeModeNotifier].
 @ProviderFor(ThemeModeNotifier)
 final themeModeNotifierProvider =
-    AutoDisposeNotifierProvider<ThemeModeNotifier, ThemeMode>.internal(
+    NotifierProvider<ThemeModeNotifier, ThemeMode>.internal(
   ThemeModeNotifier.new,
   name: r'themeModeNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -21,6 +21,6 @@ final themeModeNotifierProvider =
   allTransitiveDependencies: null,
 );
 
-typedef _$ThemeModeNotifier = AutoDisposeNotifier<ThemeMode>;
+typedef _$ThemeModeNotifier = Notifier<ThemeMode>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
